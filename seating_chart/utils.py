7202292820a600674ec3ba3a9c7f0121d69df9a9
tsr10 <theodore.rogers@gmail.com>
2014-12-19 10:18:00 -0500

    switched to tuples

--- seating_chart/utils.py
+++ seating_chart/utils.py
@@ -29,47 +29,47 @@ def make_new_seating_chart(diners, manually_placed_diners, randomly_placed_diner
     i = 0
     while i < NUMBER_OF_RANDOM_ARRANGEMENTS_TO_TRY:
         diner_list = randomly_arrange_diners(diners=diners, manually_placed_diners=manually_placed_diners, randomly_placed_diners=randomly_placed_diners)
-        return_value, chart = add_one_more_diner(current_chart=[], possible_neighbors=possible_neighbors, diner_list=diner_list, manually_placed_diners=manually_placed_diners)
+        return_value, chart = add_one_more_diner(current_chart=[], possible_neighbors=possible_neighbors, diner_list=diner_list)
         if return_value is True:
             break
         i += 1
     if return_value is False:
-        del past_dinners[-1]
+        past_dinners.pop(-1)
         chart = make_new_seating_chart(diners=diners, manually_placed_diners=manually_placed_diners, randomly_placed_diners=randomly_placed_diners, past_dinners=past_dinners, past_seatings=past_seatings.filter(dinner__in=past_dinners))
     return chart
 
 
-def add_one_more_diner(current_chart, possible_neighbors, diner_list, manually_placed_diners):
+def add_one_more_diner(current_chart, possible_neighbors, diner_list):
     """Try adding one more diner to the table. If one more successfully added, keep calling this function until
     either the table is full or the function fails.
     """
     seat_number_to_fill = len(current_chart)
     if seat_number_to_fill == len(diner_list):
-        if (seat_number_to_fill in manually_placed_diners and 0 in manually_placed_diners) or (diner_list[seat_number_to_fill - 1] in possible_neighbors[diner_list[0]]):
+        if (diner_list[seat_number_to_fill]['manually_placed'] is True) or (diner_list[seat_number_to_fill - 1]['diner'] in possible_neighbors[diner_list[0]['diner']]):
             return True, current_chart
         else:
             return False, current_chart
-    next_choice = fill_seat_if_possible(seat_number_to_fill=seat_number_to_fill, possible_neighbors=possible_neighbors, manually_placed_diners=manually_placed_diners, diner_list=diner_list)
+    next_choice = fill_seat_if_possible(seat_number_to_fill=seat_number_to_fill, possible_neighbors=possible_neighbors, diner_list=diner_list)
     if next_choice:
         current_chart.append(next_choice)
-        return_value, chart = add_one_more_diner(current_chart=current_chart, possible_neighbors=possible_neighbors, manually_placed_diners=manually_placed_diners, diner_list=diner_list)
+        return_value, chart = add_one_more_diner(current_chart=current_chart, possible_neighbors=possible_neighbors, diner_list=diner_list)
         if return_value is True:
             return True, chart
     return False, current_chart
 
 
-def fill_seat_if_possible(seat_number_to_fill, possible_neighbors, manually_placed_diners, diner_list):
+def fill_seat_if_possible(seat_number_to_fill, possible_neighbors, diner_list):
     """Returns the diner who should sit in the seat number specified by seat_number_to_fill.
 
     Check to see if the user chose to place a diner manually in this seat. If two consecutive diners are manually placed, omit the check
     to possible_neighbors. If the next diner is randomly placed, check the possible_neighbor dictionary to make sure that they can sit there.
     If no randomly placed diners qualify, or if the manually placed diner cannot sit to its already-placed neighbor, return None.
     """
-    if seat_number_to_fill in manually_placed_diners:
-        if (manually_placed_diners[seat_number_to_fill] in possible_neighbors[diner_list[seat_number_to_fill - 1]]) or (seat_number_to_fill - 1) in manually_placed_diners:
-            next_choice = manually_placed_diners[seat_number_to_fill]
-    elif diner_list[seat_number_to_fill] in possible_neighbors[diner_list[seat_number_to_fill - 1]]:
-        next_choice = diner_list[seat_number_to_fill]
+    if diner_list[seat_number_to_fill]['manually_placed'] is True:
+        if (diner_list[seat_number_to_fill]['diner'] in possible_neighbors[diner_list[seat_number_to_fill - 1]['diner']]) or diner_list[seat_number_to_fill - 1]['manually_placed'] is True:
+            next_choice = diner_list[seat_number_to_fill]['diner']
+    elif diner_list[seat_number_to_fill]['diner'] in possible_neighbors[diner_list[seat_number_to_fill - 1]['diner']]:
+        next_choice = diner_list[seat_number_to_fill][0]
     else:
         next_choice = None
     return next_choice
@@ -77,9 +77,6 @@ def fill_seat_if_possible(seat_number_to_fill, possible_neighbors, manually_plac
 
 def configure_seating_flags(seating_list):
     """Loads each diner's neighbors and sets the is_head and is_foot flags.
-
-    :param seating_list: The list of seatings we want to set the variables for
-    :type seating_list: [Seating]
     """
     for i, seating in enumerate(seating_list):
         seating_list[i].left_neighbor = seating_list[i-1]
@@ -100,8 +97,8 @@ def randomly_arrange_diners(randomly_placed_diners, manually_placed_diners, dine
     shuffle(randomly_placed_diners)
     for i in xrange(0, len(diners)):
         if i in manually_placed_diners:
-            new_diner_list.append(manually_placed_diners[i])
+            new_diner_list.append({'diner': manually_placed_diners[i], 'manually_placed': True})
         else:
-            new_diner_list.append(randomly_placed_diners[0])
+            new_diner_list.append({'diner': randomly_placed_diners[0], 'manually_placed': False})
             randomly_placed_diners.pop(0)
     return new_diner_list
